4d8d232962ee912ae3853c87fdaff3a283a65d96
junhee_lee2 <junhee_lee2@tmax.co.kr>
2023-10-30 18:45:02 +0900

    paint color 1

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const canvas = document.querySelector("canvas");
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
-
-const colors = [
-  "#1abc9c",
-  "#2ecc71",
-  "#3498db",
-  "#9b59b6",
-  "#34495e",
-  "#f1c40f",
-  "#e67e22",
-  "#e74c3c",
-  "#95a5a6",
-];
 
 let isPainting = false;
 
 function onMouseMove(event) {
   if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
     ctx.stroke();
     return;
   }
@@ -41,9 +28,15 @@ function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", endPainting);
 canvas.addEventListener("mouseleave", endPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
